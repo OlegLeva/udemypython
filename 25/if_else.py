281323d41cb8ecd8a295f72ac608a8f3e70ef0c0
OlegLeva <oleg.levitskiy@gmail.com>
2020-11-03 12:59:51 +0200

    25-26

---
+++ 25/if_else.py
@@ -0,0 +1,12 @@
+# number = int(input('Enter any number '))
+# if number == 7:
+#     print( '7 is a lucky number! Today is your lucky day!')
+# else:
+#     print('Thank you! Have a nice day!')
+
+
+numb = int(input('Enter an integer number '))
+if numb % 2 == 0:
+    print('The number is even')
+elif numb % 2 != 0:
+    print('The number is odd')
---
+++ 26/for_range.py
@@ -0,0 +1,5 @@
+sum_number = 0
+for number in range(10, 31):
+    if number % 2 == 0:
+        sum_namber = sum_number + number
+print(sum_number)
