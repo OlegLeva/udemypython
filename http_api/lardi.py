187f1d650c85722967f050f22ca49f2a76d99de7
OlegLeva <oleg.levitskiy@gmail.com>
2020-12-03 16:07:32 +0200

    HTTP_API

--- http_api/lardi.py
+++ http_api/lardi.py
@@ -1,12 +1,27 @@
 import requests
 
-obl = ','.join([str(i) for i in range(1, 25)])
+obl = ','.join([str(i) for i in range(1, 35)])
 url = 'https://api.lardi-trans.com/v2/references/areas?ids=' + obl
-
 
 respons = requests.get(url, headers={"Accept": "application/json", "Authorization": "2C3BC10BCBR000000197"})
 
 data = respons.json()
 
-for i in data:
-    print(i)
+
+def get_obl_by_id(id_number):
+    for i in data:
+        if i['id'] == id_number:
+            return i['name']
+
+
+print('Для выхода из программы используйте цифру 1')
+while True:
+    id = int(input('Введите id области '))
+    if id == 1:
+        print('Спасибо, до свидания)')
+        break
+    elif id not in range(15, 40):
+        print('Нет такого id')
+
+    else:
+        print(get_obl_by_id(id))
---
+++ http_api/usgs.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+data_start = input('Введите дату начала периода в формате "ГГГГ-ММ-ДД": ')
+data_end = input('Введите дату конца периода в формате "ГГГГ-ММ-ДД": ')
+latit = input('Укажите широту, которая будет использоваться для поиска радиуса: ')
+long = input('Укажите долготу, которая будет использоваться для поиска радиуса: ')
+radius = input('Введите радиус зоны в км.: ')
+magnitude = input('Укажите минимальную магнитуду: ')
+
+url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?'
+respons = requests.get(url, headers={"Accept": "application/json"},
+                       params={
+                           'format': 'geojson',
+                           'starttime': data_start,
+                           'endtime': data_end,
+                           'latitude': latit,
+                           'longitude': long,
+                           'maxradiuskm': radius,
+                           'minmagnitude': magnitude
+                       })
+
+data = respons.json()
+with open('file_json.json', 'w') as f:
+    json.dump(data, f, indent=4)
+for n, d in enumerate(data['features'], 1):
+    print(f'{n}. Place: {d["properties"]["place"]} Magnitude: {d["properties"]["mag"]}')
